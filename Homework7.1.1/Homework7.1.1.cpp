4cfdf20b75b9f519032851439f2197b1c5fc4d2c
Vladimir Romanov <valgard5@mail.ru>
2023-03-12 15:55:59 +0300

    initial commit

---
+++ Homework7.1.1/Homework7.1.1.cpp
@@ -0,0 +1,53 @@
+﻿// Homework7.1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+class Window
+{
+protected:
+    int max_people = 0;
+    int people = 1;
+public:
+    Window(int max_people_)
+    {
+        max_people = max_people_;
+    }
+    void income()
+    {
+        while (people != max_people)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            ++people;
+            std::cout << people << " ";
+        }
+    }
+    void outcome()
+    {
+        while (people != 0)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            --people;
+            std::cout << people << " ";
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+
+    int people_num = 5;
+
+    Window window(people_num);
+
+    std::cout << "Current workload: ";
+    std::thread t1(&Window::income, &window);
+    std::thread t2(&Window::outcome, &window);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
